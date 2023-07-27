08995278ebeb8adc608e5849c61e87b9ff7f59ce
Austin Bolton <aibolt3@gmail.com>
2023-07-27 13:41:48 -0500

    added rollbar

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(`public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'a998601f1c814ca7be85b37e7ff873cc',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const { getCompliment, getFortune, getMagicEight, getMovie, addMovie, editMovie, deleteMovie,
 getTodo, addTodo, editTodo, deleteTodo} = require('./controller')
 
